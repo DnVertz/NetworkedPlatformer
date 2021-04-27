a7c1fd655a194ffef0779492f25bd1a9640b13f0
Dennis <dennisvertyzas@gmail.com>
2021-04-27 12:46:57 +1000

    Re-added leave functionality

--- network.py
+++
@@ -1,5 +0,0 @@
-import pygame
-import threading, uuid, asyncio
-
-HOST = '127.0.0.1'
-PORT = 8008
--- server.py
+++ server.py
@@ -1,46 +1,22 @@
 import threading, uuid, asyncio
 import socket
 import socketserver
-
-
-PORT = 8008
 #Jank cannot describe this solution
-#s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-#s.connect(("8.8.8.8", 8008))
-#HOST = str(s.getsockname()[0])
-#s.close()
+s.connect(("8.8.8.8", 8008))
+HOSTname = str(s.getsockname()[0])
+s.close()
+PORT = 8008
 HOST = ''
-
-
 players = []
 players_lock = threading.Lock()
 
-"""
-	Packets
-
-	Inbound (to server)
-
-	Player Pos: sent on player position change, scope: player
-		pos;<X>;<Y>
-	
-	Outbound (to clients)
-
-	Player Init: sent on join, scope: player
-		init;<ID>;<X>;<Y>
-	Player Join: sent on join, scope: all
-		join;<ID>;<X>;<Y>
-	Player Pos: sent on player positon change, scope: all
-		pos;<ID>;<X>;<Y>
-
-"""
 
 def broadcast(data):
 	for player in players:
-
 		try:
-			player.conn.write(data)
-			#player.conn.drain()
+			player.socket.write(data)
 		except:
 			pass
 
@@ -71,19 +47,18 @@ def sendPlayerPos(player,addr):
 	player.socket.sendto(data.encode('UTF-8'),addr)
 
 
+
 class PositionUpdate:
 	def __init__(self, pid, x: int, y: int):
 		self.id = pid
 		self.x = x
 		self.y = y
 	def updatePlayer(self):
-		#players_lock.acquire()
 		for i in range(len(players)):
 			if players[i].id == self.id:
 				players[i].x = self.x
 				players[i].y = self.y
 				break
-		#players_lock.release()
 	def forwardToClients(self):
 		data = "pos;"+str(self.id)+";"+str(self.x)+";"+str(self.y)+"\n"
 		broadcast(data.encode('UTF-8'))
@@ -108,20 +83,16 @@ class Player:
 
 
 class MyUDPHandler(socketserver.DatagramRequestHandler):
-
 	def handle(self):
-		#print(self.request)
 		data = self.request[0].strip()
 		socket = self.request[1]
 		socket.settimeout(1.0)
 		data = data.decode()
 		split = data.split(";")
+
 		if split[0] == 'join':
-			#print("ya")
 			player = Player(socket,self.client_address)
 			playerID = player.id
-			#print (playerID)
-
 			players.append(player)
 			sendPlayerInit(player, self.client_address,socket) #send init id and pos to player
 			for p in players:
@@ -129,45 +100,28 @@ class MyUDPHandler(socketserver.DatagramRequestHandler):
 				if p is not player:
 					sendPlayerSpawn(p,player.addr)
 					sendPlayerSpawn(player,p.addr)
-
 				else:		
 					sendPlayerSpawn(player,p.addr)
-				#sendPlayerJoin(player) #broadcast init id and pos to all players
 		
 		elif split[0] == 'pos':
-
 			for i in range(len(players)):
-				print(players[i].id)
 				if str(players[i].id) == split[1]:
-
 					players[i].x = split[2]
 					players[i].y = split[3]
-
 					for p in players:
 						if str(p.id) != str(players[i].id):
-							print(p.id)
-							print(players[i].id)
 							sendPlayerPos(players[i],p.addr)
-
-		
-
-		
-
-				
-
-				
-
+							
+		elif split[0] == 'leave':
+			for i in range(len(players)):
+				if str(players[i].id) == split[1]:
+					sendPlayerLeave(players[i].id)
+					players.remove(players[i])
+							
 
 async def main():
     server = socketserver.UDPServer((HOST,8008), MyUDPHandler)
-    #server._handle_request_noblock()
-    #dserver.max_packet_size = 8192*2
-    #server.setblocking(False)
-
-
-    #
-    #addr = server.sockets[0].getsockname()
-    print(f'Serving on '+str(HOST)+" "+str(8008))
+    print(f'Serving on '+str(HOSTname)+" "+str(8008))
     server.serve_forever()
 
 asyncio.run(main())
