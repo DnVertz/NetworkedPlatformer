6fc69f0d82546cf2aaa02edbb37d9f3044702ac7
Dennis <dennisvertyzas@gmail.com>
2021-08-04 17:46:00 +1000

    Phys change 7

--- actor.py
+++ actor.py
@@ -44,7 +44,7 @@ class actor:
 		if self.vx is not 0 and self.vy == 0:
 			self.vx *= 0.75
 		elif self.vy is not 0:
-			self.vx *= 0.985
+			self.vx *= 0.95
 
 		#gravity/falling/floor collisons
 		if self.inBounds(self.x, self.y+g+self.vy) == False:
